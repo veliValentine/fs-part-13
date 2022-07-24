1291e6bbae13b7d810fbc8d68e7694bab000935c
unknown <nikkevanukas1995@gmail.com>
2022-07-24 14:18:21 +0300

    13.2

---
+++ commands.sql
@@ -0,0 +1,21 @@
+/* 13.2 */
+/* Create Blogs table */
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    author text,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes integer DEFAULT 0
+);
+/* 13.2 */
+/* Insert blogs */
+INSERT INTO blogs
+  (author, url, title)
+VALUES
+  ('testUser1', 'https://example.com', 'Test title 1'),
+  ('testUser2', 'https://example.com', 'Test title 2');
+/*  */
+/*  */
+
+/*  */
+/*  */
